70526dee3041b6e4b56cd5f681422ded0c6b1db8
Dalton Altstaetter <daltstaetter@gmail.com>
2019-10-30 22:12:59 -0500

    Add variable extraction
    Signed-off-by: Dalton Altstaetter <daltstaetter@gmail.com>

--- fixpoint_parser.py
+++ fixpoint_parser.py
@@ -6,7 +6,54 @@ ver = python_version()
 if ver.startswith('2'):
     sys.exit('Run with python3')
 
-import os,sys, pathlib
+import os,sys, pathlib, re, scanf
+
+
+
+
+def get_variable(input_line):
+    var_dict = {}
+    var_re = re.compile("fix_t \w[,\s\w]*") # gets all var declarations
+    #Qformat = re.compile("fix_t\s+\w[,\s\w]*[\s=]*[x\d\sa-fA-F]*;\s*/[\*/]+\s*[Qq][\d]+\.[\d]+[\s\*]*[/]*") # fix_t a[,b,c,d][ = 0x123]; // Q3.28 
+    match = var_re.match(input_line)
+    if match:
+        match_str = match.group()
+        in_vars = match_str.split('fix_t')[1].strip() # removes 'fix_t'
+
+        for i in in_vars.split(','):
+            var_dict[i.strip()] = None
+
+    print(var_dict)
+
+    # get q_format
+    if '//' in input_line:
+        var_qformat_str = input_line.split('//')[1].strip()
+        print(var_qformat_str)
+        while var_qformat_str[0] is not ('q' or 'Q') and ('q' or 'Q') in var_qformat_str[1:]:
+            var_qformat_str = var_qformat_str[1:].strip()
+        
+    elif '/*' in input_line:
+        var_qformat_str = input_line.split('/*')[1].strip()
+        print(var_qformat_str)
+        while var_qformat_str[0] is not ('q' or 'Q') and ('q' or 'Q') in var_qformat_str[1:]:
+            var_qformat_str = var_qformat_str[1:].strip()
+    else:
+        var_qformat_str = None;
+
+
+    qformat_re = re.compile("[Qq][\d]+\.[\d]+")
+    qformat = qformat_re.match(var_qformat_str)
+    if qformat:
+        (_,nInt,nFraction) = scanf.scanf("%c%d.%d",qformat.group())    
+    print(nInt,nFraction)
+
+
+##
+
+
+
+
+
 
 #sys.argv[0] = 'fixpoint_parser.py'
 #sys.argv.append('filename.c')
@@ -40,6 +87,7 @@ with path.open() as in_file:
         if line.strip().startswith(r'//'):
             continue
         elif line.strip().startswith(r'fix_t'):
+            get_variable(line.strip())
             pass # identify variables & assign format
         elif 'FMUL3' in line.strip():
             found_fmul3 = True;#print(line + " found FMUL",end='')
