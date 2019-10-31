15e77b624e6c00430bdc5679a6f5a309d4d9b91e
Dalton Altstaetter <daltstaetter@gmail.com>
2019-10-31 05:04:36 -0500

    Fixed bug where match.split matches not just the '3' in FMUL3 but also the operand for 3[0-9]
    Signed-off-by: Dalton Altstaetter <daltstaetter@gmail.com>

--- fixpoint_parser.py
+++ fixpoint_parser.py
@@ -47,50 +47,44 @@ def get_variable(input_line, var_dict):
     return var_dict
 ##
 def check_format(input_line, var_dict):
-    FMUL_re = re.compile('FMUL3\(\s*\w+\s*,\s*\w+\s*,\s*\w+\s*\)')
+    FMUL_re = re.compile('FMUL3\(\s*\w+\s*,\s*\w+\s*,\s*[-]*\w+\s*\)')
     op1_re = re.compile('\(\s*\w+\s*,')
     op2_re = re.compile(',\s*\w+\s*,')
     op3_re = re.compile(',\s*\w+\s*\)')
-    
-    if FMUL_re.search(input_line):
-        fmul_list = FMUL_re.findall(input_line)
-        for match in fmul_list:
-            a=match.split('3')[1].split(',')
-            print(a)
-            for i,val in enumerate(a):
-                a[i] = a[i].strip()
-            print(a)
-            a = ','.join(a)
-            #print(a)
-            #operands = scanf.scanf('(%s,%s,%s)',a)
-            #operands = match.split('3')[1]
-            #print(operands)
-            #print(a[0], type(a[0]))
-            #op1 = a''.join(scanf.scanf('(%s',a[0]))
-            #print(a[1], type(a[1]))
-            #print(a[2], type(a[2]))
-            op2 = a[1]#''.join(a[1])
-            #op3 = ''.join(scanf.scanf('%s)',a[2]))
+    try:
+        print("in check_format: " + input_line)
+        if FMUL_re.search(input_line):
+            fmul_list = FMUL_re.findall(input_line)
+            for match in fmul_list:
+                operands=match.split('FMUL3')[1].split(',')
+                print(operands)
+                for i,val in enumerate(operands):
+                    operands[i] = operands[i].strip()
+                operands = ','.join(operands)
+                (op1,op2,op3) = scanf.scanf('(%s,%s,%s)', operands)
 
-            try:
-                op3 = int(op3)
-            except ValueError:
-                sys.exit('Value Error: operand 3 not a numeric literal\nInput line: {}'.format(input_line))
+                try:
+                    op3 = int(op3)
+                    print(op3)
+                except ValueError:
+                    sys.exit('Value Error: operand 3 not a numeric literal\nInput line: {}'.format(input_line))
 
-    dest_re = re.compile('\s*\w\s*=')
+        dest_re = re.compile('\s*\w\s*=')
 
-    if FMUL_re.search(input_line) and dest_re.match(input_line):
-        dest_str = dest_re.match(input_line).group()[:-1].strip()
-        if (dest_str and op1 and op2) in var_dict.keys():
-            frac_bits = var_dict[op1].fraction + var_dict[op2].fraction - op3
-            if var_dict[dest_str].fraction is not frac_bits:
-                print("Error: result expected Q{0} from Q{1} * Q{2}".format(var_dict[dest_str].qformat(),
-                                                                            var_dict[op1].qformat(),
-                                                                            var_dict[op2].qformat()
-                                                                            )
-                      )
-        else:
-            pass # TODO figure out how to handle misses
+        if FMUL_re.search(input_line) and dest_re.match(input_line):
+            dest_str = dest_re.match(input_line).group()[:-1].strip()
+            if (dest_str and op1 and op2) in var_dict.keys():
+                frac_bits = var_dict[op1].fraction() + var_dict[op2].fraction() - op3
+                if var_dict[dest_str].fraction() is not frac_bits:
+                    print("Error: result expected Q{0} from Q{1} * Q{2}".format(var_dict[dest_str].qformat(),
+                                                                                var_dict[op1].qformat(),
+                                                                                var_dict[op2].qformat()
+                                                                                )
+                          )
+            else:
+                pass # TODO figure out how to handle misses
+    except Exception:
+        sys.exit('Input line: {}'.format(input_line))
             
 ##
 
